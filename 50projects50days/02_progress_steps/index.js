677094c8d20b85c33d59e516205ce365b662a702
Thomas Verderesi <tverderesi@gmail.com>
2022-07-24 17:17:45 -0300

    file:   50projects50days/02_progress_steps/index.html #	new file: 50projects50days/02_progress_steps/index.js #	new file: 50projects50days/02_progress_steps/style.css # Little update.

---
+++ 50projects50days/02_progress_steps/index.js
@@ -0,0 +1,28 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive < circles.length ? currentActive++ : circles.length;
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive > 1 ? currentActive-- : 1;
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    idx < currentActive ? circle.classList.add("active") : "";
+    //   circles.classList.remove("active");
+  });
+
+  const actives = document.querySelectorAll(".active");
+  progress.style.width = `${
+    ((actives.length - 1) / (circles.length - 1)) * 100
+  }%`;
+}
