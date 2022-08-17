0c55aa622b6a704ea0c352ba432715c63bd48de0
Thomas Verderesi <tverderesi@gmail.com>
2022-08-17 19:34:06 -0300

    Added a loading bar.

--- 50projects/05/index.js
+++ 50projects/05/index.js
@@ -1,6 +1,9 @@
 const LoadText = $(".loading-text");
 const bg = $(".bg");
 const title = $("h1");
+const load_anim = $(".loading-anim");
+const width = load_anim.width();
+const progress_bar = $(".progress-bar");
 
 let load = 0;
 
@@ -10,6 +13,9 @@ let interval = setInterval(() => {
   LoadText.css("opacity", 1 - load / 100);
   title.css("opacity", load / 100);
   bg.css("filter", `blur(${30 - (30 * load) / 100}px)`);
+  load_anim.css("width", (width * load) / 100);
+  load_anim.css("opacity", 1 - load / 100);
+  progress_bar.css("opacity", 1 - load / 100);
 
   if (load > 99) {
     clearInterval(interval);
