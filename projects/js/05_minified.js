d3cb3421c450905e1c711f800068364cd9f4bb87
Thomas Verderesi <tverderesi@gmail.com>
2022-08-26 02:15:12 -0300

    fixed loading bar.

--- projects/js/05_minified.js
+++ projects/js/05_minified.js
@@ -1,20 +1,22 @@
 const LoadText = $(".loading-text");
 const bg = $(".bg");
 const title = $("h1");
+
 const load_anim = $(".loading-anim");
-const width = load_anim.width();
 const progress_bar = $(".progress-bar");
 
 let load = 0;
 $("body").on("click", function (param) {
   loader = new Promise(resolve => {
+    const width = document.documentElement.clientWidth / 2;
     let interval = setInterval(() => {
       load++;
       LoadText.html(`${load}%`);
       LoadText.css("opacity", 1 - load / 100);
       title.css("opacity", load / 100);
       bg.css("filter", `blur(${30 - (30 * load) / 100}px)`);
-      load_anim.css("width", (width * load) / 100);
+
+      load_anim.css("width", 0.01 * width * load);
       load_anim.css("opacity", 1 - load / 100);
       progress_bar.css("opacity", 1 - load / 100);
       console.log(interval);
@@ -28,8 +30,8 @@ $("body").on("click", function (param) {
 });
 
 function unload() {
+  const width = document.documentElement.clientWidth / 2;
   let interval2 = setInterval(() => {
-    console.log(load);
     load--;
     LoadText.html(`${load}%`);
     LoadText.css("opacity", 1 - load / 100);
